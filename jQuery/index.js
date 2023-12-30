31cbf76f10a786c9a2340bdf76d8e7f3c0999d04
Rehan George <rehan.george@gmail.com>
2023-12-30 19:27:48 +0530

    Animations

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,3 +1,37 @@
 $("h1").addClass("big-title margin-50");
 
 $("button").text("Don't Click Me");
+
+$("a").attr("href", "https://www.yahoo.com");
+
+$("h1").click(function () {
+  $("h1").toggleClass("active");
+});
+
+$("button").click(function () {
+  //   $("h1").fadeToggle();
+  //   $("h1").slideToggle();
+  //   $("h1").animate({ opacity: 0.5 });
+  $("h1").slideUp().slideDown().animate({ opacity: 0.5 });
+});
+
+$(document).keydown(function (event) {
+  $("h1").text(event.key);
+});
+
+/*
+$("h1").on("mouseover", function () {
+  $("h1").css("color", "purple");
+});
+
+
+$("h1").before("<button>New</button>");
+
+$("h1").after("<button>New</button>");
+
+$("h1").prepend("<button>New</button>");
+
+$("h1").append("<button>New</button>");
+
+$("button").remove();
+*/
